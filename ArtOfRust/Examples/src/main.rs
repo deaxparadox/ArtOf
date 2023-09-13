1d435f131965c8f622b499e38c4fc2b24059775a
deaxparadox <nitish136900kushwaha@gmail.com>
2023-09-13 07:49:59 +0530

    rust : hashmaps

--- ArtOfRust/Examples/src/main.rs
+++ ArtOfRust/Examples/src/main.rs
@@ -9,15 +9,5 @@ use structure as st;
 use enump as e;
 
 fn main() {
-    // println!("Hello, world!");
-    // sl::main();
-    // sl::slice_array();
-
-    // st::tuple::tuple_information::main();
-    // st::rectangle::rectangle_with_struct::main();
-    // st::method::normal::main();
-
-    // e::enum_variable::main();
-    // e::main();
     collections::main();
 }
